8eec705c106654802022de52e37c3746b17d810c
Devolek <degustator.chudes@gmail.com>
2020-07-02 12:04:46 +0300

    fixed dialogs, fixed unread count, fixed search friends

--- src/main/java/com/skillbox/socialnetwork/main/dto/dialog/DialogFactory.java
+++ src/main/java/com/skillbox/socialnetwork/main/dto/dialog/DialogFactory.java
@@ -51,7 +51,7 @@ public class DialogFactory {
                 messageList.size(),
                 offset,
                 limit,
-                messageList.size() > 0 ? formatMessages(messageList, user) : null
+                messageList.size() > 0 ? formatMessages(messageList, user) : new ArrayList<>()
         );
     }
 
