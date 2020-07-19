7428138f2d9b7f61c1242286f06aa7ff86f6f229
Альчин Юрий <avtorutet321@gmail.com>
2020-07-19 20:44:28 +0300

    Added spring.boot.admin + spring.boot.actuator. Fix AOP LoggingAspect

---
+++ social-network/src/main/java/com/skillbox/socialnetwork/main/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.skillbox.socialnetwork.main.config;
+
+import com.skillbox.socialnetwork.main.security.jwt.JwtConfigurer;
+import com.skillbox.socialnetwork.main.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final JwtTokenProvider jwtTokenProvider;
+    private static final String LOGIN_ENDPOINT = "/api/v1/auth/login";
+
+    @Autowired
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .cors()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                    .antMatchers(
+                            "/api/v1/auth/*",
+                            "/api/v1/account/register",
+                            "/api/v1/account/password/recovery",
+                            "/api/v1/account/password/set",
+                            "/api/v1/platform/languages",
+                            "/api/v1/storage/*"
+                    ).permitAll()
+                    .antMatchers("/api/v1/**").hasRole("USER")
+                    .antMatchers("/actuator/","/actuator/**").hasRole("USER")
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider))
+                .and().exceptionHandling().authenticationEntryPoint(Http401EntryPoint.unauthorizedHandler());
+    }
+}
--- src/main/java/com/skillbox/socialnetwork/main/dto/friends/FriendsResponseFactory.java
+++
@@ -1,9 +0,0 @@
-package com.skillbox.socialnetwork.main.dto.friends;
-
-import com.skillbox.socialnetwork.main.dto.universal.Dto;
-
-public class FriendsResponseFactory {
-    public static Dto getMessage(String message) {
-        return new MessageDto(message);
-    }
-}
