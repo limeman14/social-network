c19f8789b9b7d9595c43445a412816debbee594c
Andrey <a.v.karmashov@gmail.com>
2020-07-24 17:10:10 +0300

    Deploy version 0.3.8

---
+++ scripts/deploy-social-network.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+mvn clean package
+
+echo 'Copy files...'
+
+scp -i ~/.ssh/id_rsa \
+    social-network/target/social-network-1.1-SNAPSHOT.jar \
+    javapro@176.118.165.204:/home/javapro/
+
+echo 'Restart server...'
+
+ssh -i ~/.ssh/id_rsa javapro@176.118.165.204 << EOF
+pgrep java | xargs kill -9
+nohup java -jar social-network-1.1-SNAPSHOT.jar > log.txt &
+EOF
+
+echo 'Bye'
