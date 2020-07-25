9912c4524dc7493897b1c948544d6e4bc7798402
Alexei Sukhilin <alakai21036@gmail.com>
2020-07-25 18:35:27 +0300

    Fixed creating multiple dialogs with the same person bug

--- social-network/src/main/java/com/skillbox/socialnetwork/main/service/impl/DialogServiceImpl.java
+++ social-network/src/main/java/com/skillbox/socialnetwork/main/service/impl/DialogServiceImpl.java
@@ -27,7 +27,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
@@ -44,7 +43,8 @@ public class DialogServiceImpl implements DialogService {
     private final NotificationService notificationService;
 
     @Autowired
-    public DialogServiceImpl(MessageRepository messageRepository, PersonService personService, DialogRepository dialogRepository, D2PRepository d2pRepository, NotificationService notificationService) {
+    public DialogServiceImpl(MessageRepository messageRepository, PersonService personService, DialogRepository dialogRepository, D2PRepository d2pRepository, NotificationService notificationService)
+    {
         this.messageRepository = messageRepository;
         this.personService = personService;
         this.dialogRepository = dialogRepository;
@@ -53,7 +53,8 @@ public class DialogServiceImpl implements DialogService {
     }
 
     @Override
-    public BaseResponseList getDialogs(String query, int offset, int limit, Person person) {
+    public BaseResponseList getDialogs(String query, int offset, int limit, Person person)
+    {
         List<Dialog> dialogList = dialogRepository.getDialogs(person, query);
         dialogList.sort(Comparator.comparing(Dialog::getMessages, (d1, d2) -> {
             Date date1 = d1.stream()
@@ -68,18 +69,32 @@ public class DialogServiceImpl implements DialogService {
     }
 
     @Override
-    public BaseResponse addDialog(DialogAddRequest request, Person currentUser) {
-        AtomicBoolean dialogAlreadyExists = new AtomicBoolean(false);
+    public BaseResponse addDialog(DialogAddRequest request, Person currentUser)
+    {
+//        AtomicBoolean dialogAlreadyExists = new AtomicBoolean(false);
         AtomicInteger dialogId = new AtomicInteger();
-        currentUser.getDialogToPeople().stream().map(DialogToPerson::getDialog).map(Dialog::getDialogToPersonList).forEach(dialogToPeople -> dialogAlreadyExists
-                .set(dialogToPeople.stream().anyMatch(dialogToPerson -> {
-                    dialogId.set(dialogToPerson.getDialog().getId());
-                    return dialogToPerson.getPerson().getId()
-                            .equals(request.getUserIds().get(0));
-                })));
-        if (dialogAlreadyExists.get()) {
+//        currentUser.getDialogToPeople().stream().map(DialogToPerson::getDialog).map(Dialog::getDialogToPersonList).forEach(dialogToPeople -> dialogAlreadyExists
+//                .set(dialogToPeople.stream().anyMatch(dialogToPerson -> {
+//                    dialogId.set(dialogToPerson.getDialog().getId());
+//                    return dialogToPerson.getPerson().getId()
+//                            .equals(request.getUserIds().get(0));
+//                })));
+
+        boolean dialogAlreadyExists = currentUser.getDialogToPeople().stream()
+                .anyMatch(dtp ->
+                        personService.findById(request.getUserIds().get(0)).getDialogToPeople().stream()
+                                .anyMatch(dialogToPerson -> {
+                                    if (dialogToPerson.getDialog().getId() == dtp.getDialog().getId())
+                                    {
+                                        dialogId.set(dtp.getDialog().getId());
+                                        return true;
+                                    } else return false;
+                                }));
+        if (dialogAlreadyExists)
+        {
             return new BaseResponse(new IdDto(dialogId.get()));
-        } else {//if not exists
+        } else
+        {//if not exists
             Dialog d = new Dialog();
             d.setDialogToPersonList(new ArrayList<>());
             d.setMessages(new ArrayList<>());
@@ -104,7 +119,8 @@ public class DialogServiceImpl implements DialogService {
 
             //dialog initializer
             Message message = new Message();
-            message.setMessageText("Пользователь " + currentUser.getFirstName() + " " + currentUser.getLastName() + " начал чат.");
+            message.setMessageText("Пользователь " + currentUser.getFirstName() + " " + currentUser
+                    .getLastName() + " начал чат.");
             message.setAuthor(currentUser);
             message.setRecipient(personService.findById(request.getUserIds().get(0)));
             message.setReadStatus(ReadStatus.SENT);
@@ -121,24 +137,28 @@ public class DialogServiceImpl implements DialogService {
     }
 
     @Override
-    public BaseResponse getUnreadMessages(Person person) {
+    public BaseResponse getUnreadMessages(Person person)
+    {
         return new BaseResponse(new CountDto(dialogRepository.countUnreadMessages(person)));
     }
 
     @Override
-    public BaseResponse deleteDialog(int dialogId) {
+    public BaseResponse deleteDialog(int dialogId)
+    {
         dialogRepository.deleteById(dialogId);
         log.info("Dialog with id {} is deleted", dialogId);
         return new BaseResponse(new IdDto(dialogId));
     }
 
     @Override
-    public BaseResponse addUsersToDialog(DialogAddRequest request) {
+    public BaseResponse addUsersToDialog(DialogAddRequest request)
+    {
         return addDialog(request, null);
     }
 
     @Override
-    public BaseResponse deleteUsersFromDialog(int dialogId, DialogAddRequest request) {
+    public BaseResponse deleteUsersFromDialog(int dialogId, DialogAddRequest request)
+    {
         Dialog dialog = dialogRepository
                 .findById(dialogId)
                 .orElse(null);
@@ -153,23 +173,28 @@ public class DialogServiceImpl implements DialogService {
     }
 
     @Override
-    public BaseResponse inviteUserToDialog(int id) {//Как линк сгенерировать??
+    public BaseResponse inviteUserToDialog(int id)
+    {//Как линк сгенерировать??
         return null;
     }
 
     @Override
-    public BaseResponse joinDialog(int id, LinkDto linkDto) {//аналогично
+    public BaseResponse joinDialog(int id, LinkDto linkDto)
+    {//аналогично
         return null;
     }
 
     @Override
-    public BaseResponseList getMessagesFromDialog(int id, String query, int offset, int limit, int fromMessageId, Person currentUser) {
+    public BaseResponseList getMessagesFromDialog(int id, String query, int offset, int limit, int fromMessageId, Person currentUser)
+    {
         Dialog dialog = dialogRepository.findById(id).orElse(null);
-        if (dialog != null) {
+        if (dialog != null)
+        {
             List<Message> list = messageRepository.getAllByMessageTextContainingAndDialog(query, dialog);
             int lastElementIndex = list.size();
             int firstElementIndex = list.size() - limit;
-            if (offset != 0){
+            if (offset != 0)
+            {
                 Message fromMessage = messageRepository.findById(fromMessageId).get();
                 int indexOfFromMessage = list.indexOf(fromMessage);
                 list = list.subList(0, indexOfFromMessage);
@@ -177,28 +202,31 @@ public class DialogServiceImpl implements DialogService {
                 firstElementIndex = lastElementIndex - limit;
 
             }
-            if (firstElementIndex >= 0){
+            if (firstElementIndex >= 0)
+            {
                 list = list.subList(firstElementIndex, lastElementIndex);
-            }
-            else if (lastElementIndex > 0) {
+            } else if (lastElementIndex > 0)
+            {
                 list = list.subList(0, lastElementIndex);
-            }
-            else list = new ArrayList<>();
+            } else list = new ArrayList<>();
             messageRepository.saveAll(list
                     .stream()
                     .filter(message -> message.getRecipient().equals(currentUser))
                     .peek(message -> message.setReadStatus(ReadStatus.READ))
                     .collect(toList()));
             return DialogFactory.getMessages(list, currentUser, offset, limit, fromMessageId);
-        } else {
+        } else
+        {
             return null;
         }
     }
 
     @Override
-    public BaseResponse addMessage(int dialogId, MessageTextDto request, Person user) {
+    public BaseResponse addMessage(int dialogId, MessageTextDto request, Person user)
+    {
         Dialog dialog = dialogRepository.findById(dialogId).orElse(null);
-        if (dialog == null) {
+        if (dialog == null)
+        {
             log.error("Adding message failed, dialog {} is null", dialogId);
             throw new NullPointerException("Dialog cannot be null");
         }
@@ -220,16 +248,19 @@ public class DialogServiceImpl implements DialogService {
     }
 
     @Override
-    public BaseResponse deleteMessage(int dialogId, int messageId) {
+    public BaseResponse deleteMessage(int dialogId, int messageId)
+    {
         messageRepository.deleteById(messageId);
         log.info("Message with id {} is deleted", messageId);
         return new BaseResponse(new MessageIdDto(messageId));
     }
 
     @Override
-    public BaseResponse editMessage(int dialogId, int messageId, MessageTextDto messageText, Person user) {
+    public BaseResponse editMessage(int dialogId, int messageId, MessageTextDto messageText, Person user)
+    {
         Message message = messageRepository.findById(messageId).orElse(null);
-        if (message == null) {
+        if (message == null)
+        {
             log.error("Message with id {} not found", messageId);
             throw new NullPointerException("Message not with id:" + messageId + " found");
         }
@@ -239,12 +270,14 @@ public class DialogServiceImpl implements DialogService {
     }
 
     @Override
-    public BaseResponse recoverMessage(int dialogId, int messageId) {
+    public BaseResponse recoverMessage(int dialogId, int messageId)
+    {
         return null;//Как?
     }
 
     @Override
-    public BaseResponse markMessageAsRead(int dialogId, int messageId, Person person) {
+    public BaseResponse markMessageAsRead(int dialogId, int messageId, Person person)
+    {
         Message message = messageRepository.findById(messageId).orElse(null);
         if (message == null)
             throw new NullPointerException("Message with id: " + messageId + " not found");
@@ -255,22 +288,26 @@ public class DialogServiceImpl implements DialogService {
     }
 
     @Override
-    public BaseResponse getUserActivityStatus(int dialogId, int userId) {
+    public BaseResponse getUserActivityStatus(int dialogId, int userId)
+    {
         return null;
     }
 
     @Override
-    public BaseResponse changeUserActivityStatus(int dialogId, int userId) {
+    public BaseResponse changeUserActivityStatus(int dialogId, int userId)
+    {
         return null;
     }
 
     @Override
-    public BaseResponse getLongPollCredentials() {
+    public BaseResponse getLongPollCredentials()
+    {
         return null;
     }
 
     @Override
-    public BaseResponse getLongPollHistory(LongpollHistoryRequest request) {
+    public BaseResponse getLongPollHistory(LongpollHistoryRequest request)
+    {
         return null;
     }
 }
