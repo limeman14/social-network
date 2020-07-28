9058cd38bbb2b9f5a1b353d78a59b8827b88d6c0
Andrey <a.v.karmashov@gmail.com>
2020-07-28 21:22:51 +0300

    Deploy version 0.4

--- scripts/deploy-social-network.sh
+++ scripts/deploy-social-network.sh
@@ -11,7 +11,7 @@ scp -i ~/.ssh/id_rsa \
 echo 'Restart server...'
 
 ssh -i ~/.ssh/id_rsa javapro@176.118.165.204 << EOF
-pgrep java | xargs kill -9
+#pgrep java | xargs kill -9
 nohup java -jar social-network-1.1-SNAPSHOT.jar > log.txt &
 EOF
 
