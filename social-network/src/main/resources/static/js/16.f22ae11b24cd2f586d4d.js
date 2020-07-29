06ff0aa0e35c957d3d73b819da3d8c74cc15543c
Devolek <degustator.chudes@gmail.com>
2020-07-29 10:41:44 +0300

    some fix frontend

--- social-network/src/main/resources/static/js/16.f22ae11b24cd2f586d4d.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([16],{ngnF:function(t,e,s){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var n={render:function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"success-support-contacting"},[e("simple-svg",{attrs:{filepath:"/static/img/success-register.svg"}})],1)},staticRenderFns:[]};var c=s("VU/8")({name:"ContactingSupportSuccess"},n,!1,function(t){s("qq1P")},null,null);e.default=c.exports},qq1P:function(t,e){}});
-//# sourceMappingURL=16.f22ae11b24cd2f586d4d.js.map
